d4f62913574603de3fe4a4d080ec25381726bc30
Ellisvelandia <eyis619l@gmail.com>
2023-02-18 21:02:24 -0500

    mangavideoinit

---
+++ src/components/manga/mangaVideos/MangaVideo.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const MangaVideo = () => {
+  return (
+    <div>MangaVideo</div>
+  )
+}
+
+export default MangaVideo
--- src/pages/Manga.jsx
+++ src/pages/Manga.jsx
@@ -1,10 +1,12 @@
 import React from "react";
-import HeroManga from "../components/manga/magaHero/HeroManga";
+import HeroManga from "../components/manga/mangaHero/HeroManga";
+import MangaVideo from "../components/manga/mangaVideos/MangaVideo";
 
 const Manga = () => {
   return (
     <div className="my-8">
       <HeroManga />
+      <MangaVideo />
     </div>
   );
 };
